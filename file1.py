e492bd92a33e45e6e9ad97ed8dfbd2a850f12932
MikhailPimenov <misha4git@yandex.ru>
2020-05-28 00:42:42 +0300

    lecture 7 gcd(a,b), pow(a,b), pow_fast(a,b)

--- file1.py
+++ file1.py
@@ -1,33 +1,87 @@
 # lecture 7
-# fractal_rectangle
-
-import graphics as gr
-
-h = 600
-w = 600
-
-window = gr.GraphWin("RG", w, h)
-
-alpha = 0.2
+# gcd(a, b) - grand common divisor
+# pow(a, b) 
+# pow_fast(a, b)
 
 
-def fractal_rectangle(a, b, c, d, deep=10):
-    if deep < 1:
-        return
+def gcd(a, b):
+    """
+        Returns grand common divisor of a, b
+        :param a: first of two numbers
+        :param b: second of two numbers
+        :return: grand common divisor
+    """
+    if a == b:
+        return a
 
-    for m, n in (a, b), (b, c), (c, d), (d, a):
-        gr.Line(gr.Point(*m), gr.Point(*n)).draw(window)
+    elif a > b:
+        return gcd(a - b, b)
+    else:  # a < b
+        return gcd(a, b - a)
 
 
-    a1 = (a[0] * (1 - alpha) + b[0] * alpha, a[1] * (1 - alpha) + b[1] * alpha)
-    b1 = (b[0] * (1 - alpha) + c[0] * alpha, b[1] * (1 - alpha) + c[1] * alpha)
-    c1 = (c[0] * (1 - alpha) + d[0] * alpha, c[1] * (1 - alpha) + d[1] * alpha)
-    d1 = (d[0] * (1 - alpha) + a[0] * alpha, d[1] * (1 - alpha) + a[1] * alpha)
+def gcd2(a, b):
+    """
+        Returns grand common divisor of a, b
+        :param a: first of two numbers
+        :param b: second of two numbers
+        :return: grand common divisor
+    """
+    return a if b == 0 else gcd2(b, a % b)
 
-    fractal_rectangle(a1, b1, c1, d1, deep - 1)
+
+def pow(a, b):
+    """
+
+    :param a: base
+    :param b: natural or zero number, power to base be set
+    :return: base^power
+    """
+    if a < 0 or (b // 1) != b:
+        print("b is not natural")
+        return -1
+    if b == 0:
+        return 1
+    # print("power = ", b)
+    return a * pow(a, b - 1)
 
 
-set = 10
-fractal_rectangle((set, set), (w - set, set), (w - set, h - set), (set, h - set), 20)
+def pow_fast(a, b):
+    """
 
-window.wait_window()
+    :param a: base
+    :param b: natural or zero number, power to base be set
+    :return: base^power
+    """
+    if a < 0 or (b // 1) != b:
+        # print("b is not natural")
+        return -1
+    if b == 0:
+        return 1
+    # print("power fast = ", b)
+    if b % 2 == 0:
+        return pow_fast(a * a, b // 2)
+    else:
+        return a * pow_fast(a, b - 1)
+
+
+def test_gcd(gcd_function, str=""):
+    print(str)
+    print("Test #1: ", "ok" if gcd_function(10, 15) == 5 else "FAILED")
+    print("Test #2: ", "ok" if gcd_function(100, 15) == 5 else "FAILED")
+    print("Test #3: ", "ok" if gcd_function(120, 15) == 15 else "FAILED")
+    print("Test #4: ", "ok" if gcd_function(16, 40) == 8 else "FAILED")
+
+
+def test_pow(pow_function, str=""):
+    print(str)
+    print("Test #1: ", "ok" if pow_function(2, 3) == 8 else "FAILED")
+    print("Test #2: ", "ok" if pow_function(10, 2) == 100 else "FAILED")
+    print("Test #3: ", "ok" if pow_function(2, 8) == 256 else "FAILED")
+    print("Test #4: ", "ok" if pow_function(3, 3) == 27 else "FAILED")
+
+
+test_gcd(gcd)
+test_gcd(gcd2)
+test_pow(pow, "Slow power:")
+test_pow(pow_fast, "Fast power:")
