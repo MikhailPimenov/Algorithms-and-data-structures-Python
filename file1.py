b742569f1064f725749591d1987a8e332b93aaa9
MikhailPimenov <misha4git@yandex.ru>
2020-05-27 19:36:38 +0300

    lecture 6 insertion_sort, selection_sort, bubble_sort

--- file1.py
+++ file1.py
@@ -1,23 +1,80 @@
-# Sieve of Eratosthenes
+# Lecture 6
+# insertion_sort
+# selection_sort
+# bubble_sort
 
-def sieve_of_Eratosthenes(n):
+def insertion_sort(list):
     """
-    Prints prime numbers from 0 to n
-    :param n: maximum prime number can be
+    Sorts list inserting next element in sorted part of list
+    :param list: list to be sorted
     :return:
     """
-    array = [True] * n
+    N = len(list)
 
-    array[0] = False
-    array[1] = False
-    for k in range(round(n ** 0.5) + 1):
-        if array[k]:
-            for j in range(2 * k, n, k):
-                array[j] = False
-
-    for k in range(n):
-        if array[k]:
-            print(k)
+    for top in range(1, N):
+        k = top
+        while k > 0 and list[k - 1] > list[k]:
+            list[k], list[k - 1] = list[k - 1], list[k]
+            k -= 1
 
 
-sieve_of_Eratosthenes(100)
+def selection_sort(list):
+    """
+    Sorts list selecting next element in not sorted part and adding it to sorted part
+    :param list: list to be sorted
+    :return:
+    """
+    N = len(list)
+
+    for pos in range(N - 1):
+        element = list[pos]
+        index = pos
+        for k in range(pos + 1, N):
+            if list[k] < element:
+                element = list[k]
+                index = k
+        list[index], list[pos] = list[pos], list[index]
+
+
+def bubble_sort(list):
+    """
+    Sorts list moving 'lightest' element to the end of list.
+    :param list: list to be sorted
+    :return:
+    """
+    N = len(list)
+
+    for k in range(N - 1):
+        for j in range(N - 1 - k):
+            if list[j + 1] < list[j]:
+                list[j + 1], list[j] = list[j], list[j + 1]
+
+
+def test_sort(sorting_algorithm):
+    print("Testing: ", sorting_algorithm.__doc__)
+
+    list = [1, 3, 2, 5, 4]
+    list_sorted = [1, 2, 3, 4, 5]
+
+    print("Test #1: ", end="")
+    sorting_algorithm(list)
+    print("ok" if list == list_sorted else "FAILED")
+
+    list = [2, 2, 1, 0, 5]
+    list_sorted = [0, 1, 2, 2, 5]
+
+    print("Test #2: ", end="")
+    sorting_algorithm(list)
+    print("ok" if list == list_sorted else "FAILED")
+
+    list = [1, 3, 2, 5, 4, 1, 1, 2, 7, 5, 6, 6, 3]
+    list_sorted = [1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 7]
+
+    print("Test #3: ", end="")
+    sorting_algorithm(list)
+    print("ok" if list == list_sorted else "FAILED")
+
+
+test_sort(insertion_sort)
+test_sort(selection_sort)
+test_sort(bubble_sort)
