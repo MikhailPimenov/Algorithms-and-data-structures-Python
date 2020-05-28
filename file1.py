ceff686470542ce9affe4220a3af7990d4091f4f
MikhailPimenov <misha4git@yandex.ru>
2020-05-28 23:59:28 +0300

    lecture 8 generate_numbers, generate_permutations

--- file1.py
+++ file1.py
@@ -1,87 +1,62 @@
-# lecture 7
-# gcd(a, b) - grand common divisor
-# pow(a, b) 
-# pow_fast(a, b)
+# lecture 8
+# generate_numbers
+# generate_permutations
 
 
-def gcd(a, b):
+def generate_numbers(base: int, count: int = -1, prefix: list = None):
     """
-        Returns grand common divisor of a, b
-        :param a: first of two numbers
-        :param b: second of two numbers
-        :return: grand common divisor
+    Generates all numbers with length count and base count system
+    :param base: base of count system
+    :param count: amount of symbols
+    :param prefix: container for each result generated
+    :return:
     """
-    if a == b:
-        return a
+    prefix = prefix or []
 
-    elif a > b:
-        return gcd(a - b, b)
-    else:  # a < b
-        return gcd(a, b - a)
+    if count == -1:
+        count = base
+
+    if count == 0:
+        print(prefix)
+        return
+
+    for digit in range(base):
+        prefix.append(digit)
+        generate_numbers(base, count - 1, prefix)
+        prefix.pop()
 
 
-def gcd2(a, b):
+def find(element, array:list):
+    for x in array:
+        if x == element:
+            return True
+    return False
+
+
+def generate_permutations(elements: list, count: int = -1, prefix: list = None):
     """
-        Returns grand common divisor of a, b
-        :param a: first of two numbers
-        :param b: second of two numbers
-        :return: grand common divisor
-    """
-    return a if b == 0 else gcd2(b, a % b)
-
-
-def pow(a, b):
-    """
-
-    :param a: base
-    :param b: natural or zero number, power to base be set
-    :return: base^power
-    """
-    if a < 0 or (b // 1) != b:
-        print("b is not natural")
-        return -1
-    if b == 0:
-        return 1
-    # print("power = ", b)
-    return a * pow(a, b - 1)
-
-
-def pow_fast(a, b):
+    Generates all possible count permutations of list elements
+    :param elements: list of elements
+    :param count: number of elements in one combination
+    :param prefix: container of current combination
+    :return:
     """
 
-    :param a: base
-    :param b: natural or zero number, power to base be set
-    :return: base^power
-    """
-    if a < 0 or (b // 1) != b:
-        # print("b is not natural")
-        return -1
-    if b == 0:
-        return 1
-    # print("power fast = ", b)
-    if b % 2 == 0:
-        return pow_fast(a * a, b // 2)
-    else:
-        return a * pow_fast(a, b - 1)
+    prefix = prefix or [] # если этого не сделать, prefix нельзя будет использовать
+
+    if count == -1:
+        count = len(elements)
+
+    if count == 0:
+        print(prefix)
+        return
+
+    for x in elements:
+        if not find(x, prefix):
+            prefix.append(x)
+            generate_permutations(elements, count - 1, prefix)
+            prefix.pop()
 
 
-def test_gcd(gcd_function, str=""):
-    print(str)
-    print("Test #1: ", "ok" if gcd_function(10, 15) == 5 else "FAILED")
-    print("Test #2: ", "ok" if gcd_function(100, 15) == 5 else "FAILED")
-    print("Test #3: ", "ok" if gcd_function(120, 15) == 15 else "FAILED")
-    print("Test #4: ", "ok" if gcd_function(16, 40) == 8 else "FAILED")
-
-
-def test_pow(pow_function, str=""):
-    print(str)
-    print("Test #1: ", "ok" if pow_function(2, 3) == 8 else "FAILED")
-    print("Test #2: ", "ok" if pow_function(10, 2) == 100 else "FAILED")
-    print("Test #3: ", "ok" if pow_function(2, 8) == 256 else "FAILED")
-    print("Test #4: ", "ok" if pow_function(3, 3) == 27 else "FAILED")
-
-
-test_gcd(gcd)
-test_gcd(gcd2)
-test_pow(pow, "Slow power:")
-test_pow(pow_fast, "Fast power:")
+generate_numbers(2)
+generate_permutations([10, 20, 30, 40], 3)
