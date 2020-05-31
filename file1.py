20dfb421ccd042a24b676341f7c97977878222e3
MikhailPimenov <misha4git@yandex.ru>
2020-05-31 03:22:37 +0300

    lecture 10 binary_search, fibbonachi without recursion, minimal_cos_trace

--- file1.py
+++ file1.py
@@ -1,126 +1,164 @@
-# lecture 9
-# merge_sort
-# hoar_sort
+# lecture 10
+# binary_search
+# fibbonachi
+# minimal_cost_trace
 
 
-def is_sorted(array: list, ascending: bool = True):
-    sign = 2 * int(ascending) - 1
+def left_boundary(array: list, key):
+    """
+    Returns index of last element lower than key,
+    or -1 in case there are no elements in array lower than key.
+    :param array: array to be searched in
+    :param key: element left boundary is searched for
+    :return: index or -1
+    """
 
-    for index in range(len(array) - 1):
-        if sign * array[index + 1] < sign * array[index]:
-            return False
+    left = -1
+    right = len(array)
 
-    return True
-
-
-def merge(array1: list, array2: list, array: list):  # for merge_sort(...)
-    index1 = index2 = index = 0
-
-    while index1 < len(array1) and index2 < len(array2):
-        if array1[index1] <= array2[index2]:
-            array[index] = array1[index1]
-            index1 += 1
-            index += 1
+    while right - left > 1:
+        middle = left + (right - left) // 2
+        if array[middle] < key:
+            left = middle
         else:
-            array[index] = array2[index2]
-            index2 += 1
-            index += 1
+            right = middle
 
-    while index1 < len(array1):
-        array[index] = array1[index1]
-        index1 += 1
-        index += 1
-
-    while index2 < len(array2):
-        array[index] = array2[index2]
-        index2 += 1
-        index += 1
+    return left
 
 
-def merge_sort(array: list):
+def right_boundary(array: list, key):
     """
-    Sorts array using merging algorithm
-    :param array: array to be sorted
+    Returns index of first element greater than key,
+    or len(array) in case there are no elements in array greater than key.
+    :param array: array to be searched in
+    :param key: element left boundary is searched for
+    :return: index or len(array)
+    """
+
+    left = -1
+    right = len(array)
+
+    while right - left > 1:
+        middle = left + (right - left) // 2
+        if array[middle] <= key:
+            left = middle
+        else:
+            right = middle
+
+    return right
+
+
+def binary_search(array: list, key):
+    """
+    Searches key in array.
+    :param array: array to be searched in
+    :param key: element to search
+    :return: list with 3 elements: first 0 if key was not found,
+    1 if key was found. Second is left boundary - index of last element lower than key,
+    or -1 in case there were no elements in array lower than key.
+    Third is right boundary - index of first element greater than key,
+    or len(array) in case there were no elements in array greater than key.
+    """
+
+    left = left_boundary(array, key)
+    right = right_boundary(array, key)
+
+    if right - left > 1:
+        flag = 1
+    else:
+        flag = 0
+
+    result = [flag, left, right]
+
+    return result
+
+
+def fibbonachi(n: int):
+    """
+    Prints first n Fibbonachi numbers, not using recursion
+    :param n:
     :return:
     """
-    if len(array) <= 1:
-        return
+    array = [0] * n
+    array[0] = 0
+    array[1] = 1
 
-    middle = len(array) // 2
-    left = [array[i] for i in range(middle)]
-    right = [array[i] for i in range(middle, len(array))]
-
-    merge_sort(left)
-    merge_sort(right)
-
-    merge(left, right, array)
+    for index in range(2, n):
+        array[index] = array[index - 1] + array[index - 2]
+        print(array[index], '\n')
 
 
-def hoar_sort(array: list):
+def minimal_cost_trace(n, price_array: list):
     """
-    Sorts array using method of Hoar
-    :param array: array to be sorted
-    :return:
+    Calculates minimal cost to reach point n from point 0.
+    :param n: index of final point
+    :param price_array: list of prices when step into each point
+    :return: minimal cost
     """
-    if len(array) <= 1:
-        return
 
-    barrier = array[0]  # random element of array must be here
-    right = []
-    middle = []
-    left = []
+    cost_array = [0] * n
+    cost_array[0] = price_array[0]
+    cost_array[1] = price_array[1] + cost_array[0]
 
-    for index in range(len(array)):
-        if array[index] < barrier:
-            right.append(array[index])
-        elif array[index] == barrier:
-            middle.append(array[index])
-        else:  # array[index] > barrier
-            left.append(array[index])
+    for index in range(2, n):
+        cost_array[index] = price_array[index] + min(cost_array[index - 2], cost_array[index - 1])
 
-    hoar_sort(right)
-    hoar_sort(left)
-
-    for index in range(len(right)):
-        array[index] = right[index]
-    for index in range(len(middle)):
-        array[len(right) + index] = middle[index]
-    for index in range(len(left)):
-        array[len(right) + len(middle) + index] = left[index]
+    return cost_array[n - 1]
 
 
-def test_is_sorted(function):
-    print("test#1:", " ok" if function([0, 1, 2, 3, 4, 7]) else " FAILED")
-    print("test#2:", " ok" if not function([0, 10, 2, 3, 4, 7]) else " FAILED")
-    print("test#3:", " ok" if function([0, 1, 1, 1, 2, 3, 4, 7]) else " FAILED")
-    print("test#4:", " ok" if not function([10, 9, 5, 4, 4, 10], False) else " FAILED")
-    print("test#5:", " ok" if function([10, 9, 9, 5, 4, 4, 0], False) else " FAILED")
-    print("test#6:", " ok" if function([5, 5, 5, 5, 5, 5, 5]) else " FAILED")
-    print("test#7:", " ok" if function([7, 7, 7, 7, 7, 7, 7, 7, 7], False) else " FAILED")
+def test_left_boundary(algorithm):
+    array = [0, 0, 0, 0, 1, 1, 2, 2, 3, 5, 5, 5, 6, 7, 8, 8, 8, 9, 9, 9]
+    n = len(array)
+
+    print("test #1:", "ok" if algorithm(array, 0) == -1 else "FAILED")
+    print("test #2:", "ok" if algorithm(array, 1) == 3 else "FAILED")
+    print("test #3:", "ok" if algorithm(array, 2) == 5 else "FAILED")
+    print("test #4:", "ok" if algorithm(array, 4) == 8 else "FAILED")
+    print("test #5:", "ok" if algorithm(array, 9) == 16 else "FAILED")
+    print("test #6:", "ok" if algorithm(array, 15) == 19 else "FAILED")
+    print("test #7:", "ok" if algorithm(array, -5) == -1 else "FAILED")
 
 
-def test_sort(algorithm):
-    array = [1, 2, 0, 0, 0, 8, 7, 4, 3, 2, 1, 4, 6, 9, 8, 9]
-    algorithm(array)
-    print("test#1: ", "ok" if is_sorted(array) else "FAILED")
+def test_right_boundary(algorithm):
+    array = [0, 0, 0, 0, 1, 1, 2, 2, 3, 5, 5, 5, 6, 7, 8, 8, 8, 9, 9, 9]
+    n = len(array)
 
-    array = [2, 3, 4, 5, 6, 7, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5]
-    algorithm(array)
-    print("test#2: ", "ok" if is_sorted(array) else "FAILED")
-
-    array = [2, 3, 4, 5, 6, 7]
-    algorithm(array)
-    print("test#3: ", "ok" if is_sorted(array) else "FAILED")
-
-    array = [2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5]
-    algorithm(array)
-    print("test#4: ", "ok" if is_sorted(array) else "FAILED")
-
-    array = [2, 2, 2, 2, 2, 2, 2]
-    algorithm(array)
-    print("test#5: ", "ok" if is_sorted(array) else "FAILED")
+    print("test #1:", "ok" if algorithm(array, 0) == 4 else "FAILED")
+    print("test #2:", "ok" if algorithm(array, 1) == 6 else "FAILED")
+    print("test #3:", "ok" if algorithm(array, 2) == 8 else "FAILED")
+    print("test #4:", "ok" if algorithm(array, 4) == 9 else "FAILED")
+    print("test #5:", "ok" if algorithm(array, 9) == 20 else "FAILED")
+    print("test #6:", "ok" if algorithm(array, 15) == 20 else "FAILED")
+    print("test #7:", "ok" if algorithm(array, -5) == 0 else "FAILED")
 
 
-test_is_sorted(is_sorted)
-test_sort(merge_sort)
-test_sort(hoar_sort)
+def test_binary_search(algorithm):
+    array = [0, 0, 0, 0, 1, 1, 2, 2, 3, 5, 5, 5, 6, 7, 8, 8, 8, 9, 9, 9]
+
+    print("test #0:", "ok" if algorithm(array, -5) == [int(False), -1, 0] else "FAILED")
+    print("test #1:", "ok" if algorithm(array, 0) == [int(True), -1, 4] else "FAILED")
+    print("test #2:", "ok" if algorithm(array, 1) == [int(True), 3, 6] else "FAILED")
+    print("test #3:", "ok" if algorithm(array, 2) == [int(True), 5, 8] else "FAILED")
+    print("test #4:", "ok" if algorithm(array, 3) == [int(True), 7, 9] else "FAILED")
+    print("test #5:", "ok" if algorithm(array, 4) == [int(False), 8, 9] else "FAILED")
+    print("test #6:", "ok" if algorithm(array, 5) == [int(True), 8, 12] else "FAILED")
+    print("test #7:", "ok" if algorithm(array, 6) == [int(True), 11, 13] else "FAILED")
+    print("test #8:", "ok" if algorithm(array, 9) == [int(True), 16, 20] else "FAILED")
+    print("test #9:", "ok" if algorithm(array, 12) == [int(False), 19, 20] else "FAILED")
+
+
+def test_fibbonachi(algorithm):
+    algorithm(20)
+
+
+def test_minimal_cost_trace(algorithms):
+    price_array = [0, 1, 0, 0, 2, 0, 1, 0, 3, 0, 4, 0, 1, 0, 0, 1]
+
+    print("test #1: ", "ok" if algorithms(len(price_array), price_array) == 1 else "FAILED")
+
+
+test_left_boundary(left_boundary)
+test_right_boundary(right_boundary)
+test_binary_search(binary_search)
+test_fibbonachi(fibbonachi)
+test_minimal_cost_trace(minimal_cost_trace)
