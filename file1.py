a422ec84a9a226715a92f4bbc60eed7ad1857897
MikhailPimenov <misha4git@yandex.ru>
2020-06-03 16:20:31 +0300

    lecture 12 part 2 of 2 pi_function, find_substring_kmp

--- file1.py
+++ file1.py
@@ -1,60 +1,118 @@
-# lecture 12 part 1 of 2
+# lecture 12 part 2 of 2
 # part 1
 # levenshtein
 # part 2
 # pi_function
 # find_substring_kmp
 
-def levenshtein(string1: str, string2: str):
+
+def pi_function(string: str):
     """
-    Finds minimal distance between string1 and string2
-    :param string1: first str of symbols
-    :param string2: second str of symbols
+    Returns array of pi-function values for each symbol of string
+    :param string:
     :return:
     """
+    frequency = [0] * len(string)
+    frequency[0] = 0
 
-    frequency = [[0] * (len(string2) + 1) for k in range(len(string1) + 1)]
+    for index in range(1, len(string)):
+        p_index = frequency[index - 1]
 
-    for i in range(len(string1) + 1):
-        frequency[i][0] = i
-
-    for j in range(len(string2) + 1):
-        frequency[0][j] = j
-
-    for i in range(1, len(string1) + 1):
-        for j in range(1, len(string2) + 1):
-            if string1[i - 1] == string2[j - 1]:
-                frequency[i][j] = frequency[i - 1][j - 1]
+        while True:
+            if string[index] == string[p_index]:
+                if p_index > 0:
+                    frequency[index] = 1 + frequency[index - 1]
+                else:  # p_index == 0:
+                    frequency[index] = 1
+                break
             else:
-                frequency[i][j] = 1 + min(frequency[i - 1][j], frequency[i][j - 1], frequency[i - 1][j - 1])
+                if p_index > 0:
+                    p_index = frequency[p_index]
+                else:  # p_index == 0:
+                    frequency[index] = 0
+                    break
 
-    return frequency[len(string1)][len(string2)]
-
-
-def test_levenshtein(algorithm):
-    string1 = "garden is empty"
-    string2 = "father"
-
-    print("test #1:", "ok" if algorithm(string1, string2) == 13 else "FAILED")
-
-    string1 = "pig is big"
-    string2 = "big"
-    print("test #2:", "ok" if algorithm(string1, string2) == 7 else "FAILED")
-
-    string1, string2 = string2, string1
-    print("test #3:", "ok" if algorithm(string1, string2) == 7 else "FAILED")
-
-    string1 = "field"
-    string2 = "cold"
-    print("test #4:", "ok" if algorithm(string1, string2) == 3 else "FAILED")
-
-    string1 = "bottom"
-    string2 = "getters"
-    print("test #5:", "ok" if algorithm(string1, string2) == 5 else "FAILED")
-
-    string1 = "golden"
-    string2 = "cold"
-    print("test #6:", "ok" if algorithm(string1, string2) == 3 else "FAILED")
+    return frequency
 
 
-test_levenshtein(levenshtein)
+def find_substring_kmp(string: str, substring: str):
+    """
+    Returns index of first appearance substring in string
+    :param string: str to search in
+    :param substring: str to search for
+    :return: index in string
+    """
+    sum_string = substring + "#" + string;
+    pi_array = pi_function(sum_string)
+
+    result = -1
+    for k in range(len(pi_array)):
+        if pi_array[k] == len(substring):
+            result = k - len(substring) - len(substring)
+            break
+
+    return result
+
+
+def test_pi_function(algorithm):
+    print("testing pi_function:")
+
+    string = "abababa"
+    result = [0, 0, 1, 2, 3, 4, 5]
+    print("test #1:", "ok" if algorithm(string) == result else "FAILED")
+
+    string = "akeakea"
+    result = [0, 0, 0, 1, 2, 3, 4]
+    print("test #2:", "ok" if algorithm(string) == result else "FAILED")
+
+    string = "alalcda"
+    result = [0, 0, 1, 2, 0, 0, 1]
+    print("test #3:", "ok" if algorithm(string) == result else "FAILED")
+
+    string = "acdabcdabacd"
+    result = [0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 2, 3]
+    print("test #4:", "ok" if algorithm(string) == result else "FAILED")
+
+
+def test_find_substring_kmp(algorithm):
+    print("testing find_substring:")
+
+    string = "father"
+    sub_string = "garden"
+    print("test #1:", "ok" if algorithm(string, sub_string) == -1 else "FAILED")
+
+    string = "pigisbig"
+    sub_string = "big"
+    print("test #2:", "ok" if algorithm(string, sub_string) == 5 else "FAILED")
+
+    string = "dog"
+    sub_string = "horse"
+    print("test #3:", "ok" if algorithm(string, sub_string) == -1 else "FAILED")
+
+    string = ""
+    sub_string = ""
+    print("test #4:", "ok" if algorithm(string, sub_string) == 0 else "FAILED")
+
+    string = "suddenly"
+    sub_string = "suddenly"
+    print("test #5:", "ok" if algorithm(string, sub_string) == 0 else "FAILED")
+
+    string = "suddenly suddenly"
+    sub_string = "sud"
+    print("test #6:", "ok" if algorithm(string, sub_string) == 0 else "FAILED")
+
+    string = "suddenly and suddenly they got crazy and crazy"
+    sub_string = "raz"
+    print("test #7:", "ok" if algorithm(string, sub_string) == 32 else "FAILED")
+
+    string = "that terrible weather is annoying me"
+    sub_string = "noy"
+    print("test #8:", "ok" if algorithm(string, sub_string) == 27 else "FAILED")
+
+    string = "big"
+    sub_string = "pig is big"
+    print("test #9:", "ok" if algorithm(string, sub_string) == -1 else "FAILED")
+
+
+test_pi_function(pi_function)
+test_find_substring_kmp(find_substring_kmp)
