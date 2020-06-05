a047318fbff31bc98d06e65e3d49ccacf1f4a747
MikhailPimenov <misha4git@yandex.ru>
2020-06-05 22:08:02 +0300

    lecture 18 class, named_tuple, linked_list

--- file1.py
+++ file1.py
@@ -1,75 +1,56 @@
-# lecture 17
-# fibbonachi_recursive_cash
-# descrete_bag
-
-cash = [0] * 10000
-
-
-def fibbonachi_recursive_cash(n: int):
-    """
-    Returns fibbonachi number n using recursion with cash
-    :param n: number of fibbonachi to return
-    :return: number
-    """
-
-    if n > 10000:
-        return -1
-
-    if 0 <= n <= 1:
-        if cash[n] == 0:
-            cash[n] = n
-        return cash[n]
-
-    if cash[n] == 0:
-        cash[n] = fibbonachi_recursive_cash(n - 1) + fibbonachi_recursive_cash(n - 2)
-
-    return cash[n - 1] + cash[n - 2]
+# lecture 18
+# class
+# named_tuple
+# linked_list
 
 
-def discrete_bag(mass: list, price: list, mass_limit: int):
-    """
-    Returns maximum cost of items with limited sum mass
-    :param mass: list of masses
-    :param price: list of prices
-    :param mass_limit: int, limit of sum mass
-    :return: int
-    """
+class Goat:
+    legs_number = 4
 
-    cost = [[0] * (len(mass) + 1) for m in range(mass_limit + 1)]
+    def __init__(self, weight=30, height=50):
+        self.weight = weight
+        self.height = height
 
-    for i in range(len(mass) + 1):
-        for m in range(mass_limit + 1):
-            if m - mass[i - 1] >= 0:
-                cost[m][i] = max(price[i - 1] + cost[m - mass[i - 1]][i - 1], cost[m][i - 1])
-            else:
-                cost[m][i] = cost[m][i - 1]
-
-    return cost[mass_limit][len(mass)]
+    def __str__(self):
+        string = "Goat: weight = " + str(self.weight) + "  height = " + str(self.height)
+        return string
 
 
-def test_fibbonachi_recursive_cash():
-    print("testing fibbonachi:")
+def test_class_Goat():
+    notka = Goat()
+    nada = Goat(35, 50)
 
-    for k in range(20):
-        print(k, fibbonachi_recursive_cash(k))
+    goats = [notka, nada]
 
-
-# test_fibbonachi_recursive_cash()
-def test_discrete_bag(algorithm):
-    print("testing discrete_bag")
-    max_mass = 10
-
-    mass = [3, 3, 4, 5]
-    price = [30, 30, 40, 50]
-    print("test #1:", "ok" if algorithm(mass, price, max_mass) == 100 else "FAILED")
-
-    mass = [4, 9, 5, 4]
-    price = [30, 50, 40, 20]
-    print("test #2:", "ok" if algorithm(mass, price, max_mass) == 70 else "FAILED")
-
-    mass = [5, 4, 3, 2]
-    price = [200, 160, 110, 70]
-    print("test #3:", "ok" if algorithm(mass, price, max_mass) == 380 else "FAILED")
+    for k in goats:
+        print(k)
 
 
-test_discrete_bag(discrete_bag)
+from collections import namedtuple
+
+
+def test_namedtuple():
+    Point = namedtuple("Point", "x y z")
+    a1 = Point(2, 3, 4)
+    a2 = Point(-1, 0, 0)
+
+    distance = ((a1.x - a2.x) ** 2 + (a1.y - a2.y) ** 2 + (a1.z - a2.z) ** 2) ** 0.5
+    print(distance)
+
+
+def test_linked_list():
+    a = [10]
+    a.append([20])
+    a[1].append([30])
+    a[1][1].append([40])
+    a[1][1][1].append([50, None])
+
+    p = a
+    while p is not None:
+        print(p[0])
+        p = p[1]
+
+
+# test_class_Goat()
+# test_namedtuple()
+test_linked_list()
