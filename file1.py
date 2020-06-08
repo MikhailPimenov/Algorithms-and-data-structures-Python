d57e326ec98db84a3da870578e420e662295b2ec
MikhailPimenov <misha4git@yandex.ru>
2020-06-08 08:31:20 +0300

    lecture 21 python's dictionary vote

--- file1.py
+++ file1.py
@@ -1,59 +1,22 @@
-# lecture 18
-# class heap
-# heapify_fast
-# heap_sort
-
-from HeapSimple import HeapSimple
+# lecture 21
+# vote
+# using python's dictionary
 
 
-def heapify_fast(array: list):
-    heap = HeapSimple()
-    heap.values = array[:]
-    heap.length = len(array)
+def vote():
+    votes = {}
 
-    for i in range(len(array) // 2 - 1, -1, -1):
-        heap.sift_down(i)
+    for k in range(10):
+        print("Enter candidate:")
+        name = input()
 
-    for i in range(len(array)):
-        array[i] = heap.values[i]
+        if name not in votes:
+            votes[name] = 1
+        else:
+            votes[name] += 1
 
-
-def heap_sort(array: list):
-    heap = HeapSimple(array)
-
-    for i in range(len(array)):
-        array[i] = heap.extract_min()
+    for name in votes:
+        print(name, votes[name])
 
 
-def test_heapify_fast(algorithm):
-    array = [9, 8, 7, 10, 10, 10, 6, 5, 4, 3, 2, 1, 0, 2]
-    algorithm(array)
-    print(*array)
-
-
-def test_sort(algorithm, string: str = ""):
-    print("testing:", string)
-
-    array = [1, 3, 4, 1, 3, 2, 0, 5, 9, 10, 6, 5]
-    array_sorted = [0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 9, 10]
-    algorithm(array)
-    print("test #1:", "ok" if array == array_sorted else "FAILED")
-
-    array = [1, 3, 4, 6, 5]
-    array_sorted = [1, 3, 4, 5, 6]
-    algorithm(array)
-    print("test #2:", "ok" if array == array_sorted else "FAILED")
-
-    array = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    array_sorted = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    algorithm(array)
-    print("test #3:", "ok" if array == array_sorted else "FAILED")
-
-    array = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-    array_sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    algorithm(array)
-    print("test #4:", "ok" if array == array_sorted else "FAILED")
-
-
-# test_heapify_fast(heapify_fast)
-test_sort(heap_sort, "heap_sort")
+vote()
